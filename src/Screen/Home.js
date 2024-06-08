ee46f0669331a9d2800e64b5aef98d4c80b7a25a
Rasika Vikas Mozar <rasikamozar@gmail.com>
2024-06-08 09:46:21 +0530

    changes

---
+++ src/Screen/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    <h1>Hello world</h1>
+    <Footer/>
+    </>
+  )
+}
