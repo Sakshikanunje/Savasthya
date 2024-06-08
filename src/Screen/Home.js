2f639eabff1e49a29627c7e57f8a6c22d26970cf
Rasika Vikas Mozar <rasikamozar@gmail.com>
2024-06-08 10:05:55 +0530

    Changes

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import Carousel from '../Components/Carousel'
 
 export default function Home() {
   return (
     <>
     <Navbar/>
-    <h1>Hello world</h1>
+    <Carousel/>
+    
     <Footer/>
     </>
   )
